91d92999e303bfca3e527edfe16f8ba33e35ccbb
hirosuke1309 <hirosuke1309@gmail.com>
2021-03-24 11:27:58 +0900

    added localStrage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
 import './App.css';
 import React, { useState, useEffect ,useReducer} from 'react'
-// import useLocalStorage from '../hooks/useLocalStorage';
 
-const App_Key='AppKey'
 
- function App() {
-  // stateの設定 左:取得　右:設定
+// function useLocalState (key, value){
+//   const [loc, setLoc] = useState(value);
+
+//   function setState(value){
+//     localStorage.setItem(key, value);
+//     setLoc(value);
+//   }
+
+//   return [loc, setState];
+// }
+
+
+function App() {
+  // localStorage.clear()
+
+  
+  const localStorageKey = "appkey"
   const [name, setName] = useState('');
-  const [items, setItems]= useState([]);
-
+ 
+  const [items,setItems]=useState( localStorage.getItem(localStorageKey) == null ? [] : localStorage.getItem(localStorageKey).split(','));
+  // console.log( localStorage.getItem(localStorageKey).split(','))
   const handleChange = e => setName(e.target.value);
 
   const removeItem = (index) => {
@@ -17,79 +31,84 @@ const App_Key='AppKey'
     newItems.splice(index,1)
     // setItemsの中でそのstateであるitemsを入れることはない
     setItems(newItems)
+    console.log(newItems)
+    localStorage.setItem(localStorageKey, newItems)
+  };
+
+  const removeStorage = (index) =>{
+    localStorage.removeItem(index)
   };
 
 
     const click = (index) =>{
-
-    
       var elem = document.getElementById('todo'+index);
       if (document.getElementById('check'+index).checked) {
         // チェックボックスがONのときの処理
-        // elem.style.color = 'green' ;
         elem.style.textDecoration = 'line-through';
-        // console.log(elem.style)
         elem.style.fontSize = "80%";
       } else {
         // チェックボックスがOFFのときの処理
-        // console.log(elem.style.text_decoration)
         elem.style.textDecoration = 'none';
-        // console.log('false')
         elem.style.fontSize = "100%";
       }
     }
   
 
 
-  // // ローカルStorageをリロードされた際に取得
-  // const appState = localStorage.getItem(App_Key);
-  // // ?は中身がある時実行する
-  // const initialState = appState ? JSON.parse(appState) : {
-  //   items: []
-  // }
-  // setItems({items})
-  // const [items, dispatch] = useReducer(reducer, initialState)
-  
+ 
 
-  // useEffect(() => {
-  //   localStorage.setItem(App_Key, JSON.stringify(items))
-  // },[items])
+    let itemlist=
+      items.map((item, index)=> {
+        return(
+          
+          <div className='box'>
+          <input className="checkbox" type="checkbox" onChange={(e)=>{click(index)}}  id = {'check'+index} />
+
+            <div className='todo' key={index} id={'todo'+index} >{item}</div>
+            <button 
+              onClick={
+                (e) => {
+                  removeItem(index); 
+                  removeStorage(index);
+                }
+              }   
+            >削除</button>
+            
+            
+          </div>
+        );
+    })
 
 
-  
-
-
-  let itemlist=items.map((item, index)=> {
-
-    return(
-      
-      <div className='box'>
-      <input className="checkbox" type="checkbox" onChange={(e)=>{click(index)}}  id = {'check'+index} />
-
-        <div className='todo' key={index} id={'todo'+index} >{item}</div>
-        <button 
-          onClick={
-            (e) => {removeItem(index); }
-          }   
-        >削除</button>
-        
-      </div>
-    
-  )});
 
   return (
     <div className="App">
       <h1>To Do List</h1>
       <div className='search-container' >
       {/* stateの更新 */}
-      <input value={name} onChange={handleChange } />
+      <input value={name} onChange={handleChange } 
+        onKeyDown={e =>{
+          if (e.keyCode === 13) {
+            if (name=='' ) return; //空文字入力阻止
+        // 先頭に追加
+
+            items.unshift(name)
+            localStorage.setItem(localStorageKey,items)
+            setName('');
+            
+          }
+        }}
+      />
       <button onClick={() => {
         if (name=='' ) return; //空文字入力阻止
-        // items.push(name);
         // 先頭に追加
-        items.unshift(name);
+
+        items.unshift(name)
+        localStorage.setItem(localStorageKey,items)
+
         setName('');
-        console.log(items);
+        
+
       }
       } >追加</button>
           {/*  配列型じゃないとconcatできない */}
@@ -97,7 +116,7 @@ const App_Key='AppKey'
       <div className='small-container'>
       
         {/* stateの読み出し */}
-        {itemlist}
+        {itemlist } 
       </div>
       {/* <button value="更新"/> */}
     </div>
